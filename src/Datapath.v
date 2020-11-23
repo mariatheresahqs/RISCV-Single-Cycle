dca92fd0a2600d793767ea3c4b5cd54809b0030e
Maria Theresa <mariatheresahenriques@gmail.com>
2020-11-23 08:07:56 -0300

    Create Datapath.v

---
+++ src/Datapath.v
@@ -0,0 +1,13 @@
+module datapath (clk, reset, nextPC, ALUResult, instruction);
+  input wire clk, reset;
+  output wire [63:0]nextPC, ALUResult; // Alterar tamanho da instrucao ao imprimir
+  output wire[31:0]instruction;
+  wire [63:0]resultPC, sum, muxResult, muxDataResult;
+  wire [63:0]signExtend, ReadData1, ReadData2, DataTemp, WriteData, shiftValue, ReadData, PC;
+  wire [4:0]WriteReg;
+  wire ANDBranch, Zero, ANDResult;
+  wire ALUSrc, MemtoReg, MemWrite, MemRead, RegWrite, Branch;  // instrucoes de controle
+  wire [1:0]ALUOp; // instrucoes de controle
+  wire [3:0]ALUCtrl; 
+  
+endmodule
