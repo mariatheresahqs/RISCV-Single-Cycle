a72e8a273956e6814bf8467a99ffedcfaca57b4d
Maria Theresa <mariatheresahenriques@gmail.com>
2020-11-27 12:46:56 -0300

    Update InstructionMemory.v

--- src/InstructionMemory.v
+++ src/InstructionMemory.v
@@ -6,7 +6,7 @@ module InstructionMemory (PC, instruction);
   reg [31:0]MemInstr[12:0]; // edit the number of instructions of 32bits that is needed 
 
 	initial begin
-		$readmemb("binario.asm", MemInstr); // edit the number of instructions of 32bits that is needed 
+		$readmemb("instructions.asm", MemInstr); // edit the number of instructions of 32bits that is needed 
 	end
   
 	always @(PC)begin
